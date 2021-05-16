5f309d9d54016ac3aa82b19b927e91b9fb298be8
Victoria <vep002@gmail.com>
2021-05-16 17:09:44 -0500

    commit tree 4e118db58cc776059d923c99f56236fdf57df75e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+class Polygon {
+    constructor(...side){
+        this.side= side;
+    }
+
+    get countSides () {
+        return this.side.length
+    }
+
+    get perimeter () {
+        this.side.reduce((a,b) => {
+            return a+b
+        },0)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid () {
+        this.side.forEach(element => {
+            //???
+        });
+    }
+}
+
+class Square extends Polygon {
+    get isValid () {
+
+    }
+
+    get area () {
+        this.side*this.side
+    }
+}
